1cec1763eecb6140070b7c6d6a6a893cfa441770
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2025-06-01 14:32:16 -0300

    CLasses, Methods, Constructors

--- cryptomasters/converter.php
+++ cryptomasters/converter.php
@@ -1,4 +1,16 @@
 <?php
 class CryptoConverter {
+    //properties - Can be public/private/protected
+    //php uses garbage collectors
+    //Has both constructor/deconstructor
+    function __construct(public string $currencyCode) {
+        //$this->currencyCode = $currencyCode;
+    }
+
+    //methods
+    public function convert(float $value) {
 
     }
+    }
+
+    $c = new CryptoConverteer(currencyCode: "BTC");
