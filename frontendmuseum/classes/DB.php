d7a80bb62f7cf19c7551aee0461bdc83a7b2381f
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2025-06-02 11:51:04 -0300

    Databases in php continued

--- frontendmuseum/classes/DB.php
+++ frontendmuseum/classes/DB.php
@@ -1,5 +1,14 @@
 <?php
 // SQlite
 class DB {
+    function execute($query) {
+        $db = new SQLite3('../data/data.db');
+        $result = $db->query($query);
+        //return $result;
 
+        if ($result) {
+            $all = $result->fetchArray(SQLITE3_ASSOC);
+            return $all;
+        }
+    }
 }
